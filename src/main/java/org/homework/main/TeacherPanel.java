6c4cf6ea43a8e308d2abfb046f06d91e403666b8
JingsongLi <jy3161286>
2015-05-21 21:59:48 +0800

    student and teacher to mainFrame

---
+++ src/main/java/org/homework/main/TeacherPanel.java
@@ -0,0 +1,22 @@
+package org.homework.main;
+
+import org.homework.student.CatalogTree;
+import org.homework.student.ContentPanel;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+/**
+ * Created by hasee on 2015/5/21.
+ */
+public class TeacherPanel extends JPanel {
+
+
+    public TeacherPanel(){
+        setBorder(new EmptyBorder(5, 5, 5, 5));
+        setLayout(new BorderLayout(0, 0));
+       setBackground(Color.GRAY);
+    }
+}
